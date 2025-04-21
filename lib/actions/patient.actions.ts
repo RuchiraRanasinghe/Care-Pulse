8e351dd5b784062bb3d1156c226a27e27a31d8e8
Ruchira Ranasinghe <rhvruchira@gmail.com>
2025-04-21 13:23:34 +0530

    create new user done

---
+++ lib/actions/patient.actions.ts
@@ -0,0 +1,35 @@
+'use server'
+
+import { ID, Query } from "node-appwrite";
+import { users } from "../appwrite.config";
+
+// Define your user type
+type CreateUserParams = {
+  name: string;
+  email: string;
+  phone: string;
+};
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    const newUser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      undefined,
+      user.name
+    );
+
+    return newUser;
+  } catch (error: any) {
+    if (error?.code === 409) {
+      const documents = await users.list([
+        Query.equal('email', [user.email])
+      ]);
+
+      return documents?.users?.[0];
+    }
+
+    throw new Error('Failed to create user');
+  }
+};
